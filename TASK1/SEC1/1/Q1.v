b7f223be1dce346ee3d6cf0dbce2f0dfb94a7215
its-ditt <dittmc12@gmail.com>
2026-02-06 17:58:11 +0530

    header removal Q1.v
    Removed header comments from Q1.v

--- TASK1/SEC1/1/Q1.v
+++ TASK1/SEC1/1/Q1.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 25.01.2026 14:30:33
-// Design Name: 
-// Module Name: RED
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module RED(
     input  clk,
